bba8ba3ba5961629ceeb021effc26b880e05261c
iamllcoolray <llcoolray24@gmail.com>
2023-07-12 11:15:34 -0400

    successfully writes to main.lua and conf.lua

--- lib/Loverl/Create/Directory.pm
+++ lib/Loverl/Create/Directory.pm
@@ -9,6 +9,8 @@ use Loverl::Create::File_Content;
 
 has "dir_name" => (is => "rw", isa => "Str", default => "new-project");
 
+my %file_content = Loverl::Create::File_Content::file_content();
+
 my $dir = getcwd();
 
 sub project_dir($self){
@@ -23,22 +25,27 @@ sub project_subdir($self, $dir_name){
 
 sub create_file($self){
     my $FILE;
-    
-    open($FILE, ">>", project_dir($self) . "/main.lua") or die("Cannot open file: ".$!);
-    close($FILE);
 
-    open($FILE, ">>", project_dir($self) . "/conf.lua") or die("Cannot open file: ".$!);
-    close($FILE);
+    foreach my $key (keys %file_content){
+        open($FILE, ">>", project_dir($self) . "/$key.lua") or die("Cannot open file: ".$!);
+        my $value = $file_content{$key};
+        print($FILE $value);
+        close($FILE) or die("Cannot close file: ".$!);
+    }
 }
 
 sub create_dir ($self) {
-    if(-d $self->dir_name){
-        print($self->dir_name . " already exists at \$PATH=$dir/\n");
+    if(-e $self->dir_name){
+        print($self->dir_name . " already exists as a file at \$PATH=$dir/\n");
     }else{
-        mkdir(project_dir($self)) or die("Can't create directory.");
-        mkdir(project_subdir($self, "assets")) or die("Can't create directory.");
-        mkdir(project_subdir($self, "libraries")) or die("Can't create directory.");
-        create_file($self);
+        if(-d $self->dir_name){
+            print($self->dir_name . " already exists at \$PATH=$dir/\n");
+        }else{
+            mkdir(project_dir($self)) or die("Can't create directory.");
+            mkdir(project_subdir($self, "assets")) or die("Can't create directory.");
+            mkdir(project_subdir($self, "libraries")) or die("Can't create directory.");
+            create_file($self);
+        }
     }
 }
 1;
